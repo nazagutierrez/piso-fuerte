f5eeb3bf02c246564b9406efcbf80f4a72bb3995
Nazareno Gutierrez <nazarenojunin@gmail.com>
2026-01-06 21:18:04 -0300

    Custom cursor with following circle

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home/Home';
-import About from './pages/About';
-import Projects from './pages/Projects';
-import ProjectDetail from './pages/ProjectDetail';
-import Contact from './pages/Contact';
-import { Footer } from './components/footer';
-import { Navbar } from './components/Navbar';
-import gsap from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import { ScrollSmoother } from 'gsap/ScrollSmoother';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import Home from "./pages/Home/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import ProjectDetail from "./pages/ProjectDetail";
+import Contact from "./pages/Contact";
+import { Footer } from "./components/footer";
+import { Navbar } from "./components/Navbar";
+import CustomCursor from "./components/CustomCursor";
+
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrollSmoother } from "gsap/ScrollSmoother";
 
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
-ScrollSmoother.create({
-  smooth: 1, 
-  effects: true, 
-  smoothTouch: 0.1,
-});
-
 function App() {
+  useEffect(() => {
+    ScrollSmoother.create({
+      wrapper: "#smooth-wrapper",
+      content: "#smooth-content",
+      smooth: 1,
+      effects: true,
+      smoothTouch: 0.1,
+    });
+  }, []);
+
   return (
     <Router>
+      {/* ⚠️ Cursor SIEMPRE fuera del wrapper */}
+      <CustomCursor />
+
       <div id="smooth-wrapper">
         <div id="smooth-content">
           <div className="min-h-screen flex flex-col">
