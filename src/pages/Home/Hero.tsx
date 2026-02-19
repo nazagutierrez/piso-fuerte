c53a32d32152ded0b37151a2f749af47a6a5251b
Nazareno Gutierrez <nazarenojunin@gmail.com>
2026-02-18 23:57:25 -0300

    Improved hero

--- src/pages/Home/Hero.tsx
+++ src/pages/Home/Hero.tsx
@@ -7,6 +7,24 @@ import { FaInstagram, FaWhatsapp } from "react-icons/fa";
 
 gsap.registerPlugin(ScrollTrigger);
 
+const buttons = [
+  {
+    href: "tel:+542364525588",
+    icon: <FiPhone className="group-hover:text-brand-yellow transition-all" size={20} />,
+    text: "Llamar",
+  },
+  {
+    href: "https://wa.me/542364525588?text=Hola!%20vengo%20de%20la%20página%20web%20y%20quiero%20saber%20más%20sobre%20sus%20servicios.",
+    icon: <FaWhatsapp className="group-hover:text-brand-yellow transition-all" size={20} />,
+    text: "Whatsapp",
+  },
+  {
+    href: "https://www.instagram.com/constructorapisofuerte",
+    icon: <FaInstagram className="group-hover:text-brand-yellow transition-all" size={20} />,
+    text: "Instagram",
+  },
+]
+
 export default function Hero() {
   const heroSectionRef = useRef<HTMLDivElement>(null);
   const animatedHeroTextRef = useRef<HTMLSpanElement>(null);
@@ -57,7 +75,7 @@ export default function Hero() {
         opacity: 0,
         y: 30,
         duration: 1,
-        delay: 0.3,
+        delay: 0.5,
         ease: "power3.out",
         
       });
@@ -68,7 +86,7 @@ export default function Hero() {
         stagger: 0.2,
         duration: 0.8,
         ease: "power3.out",
-        delay: 0.3,
+        delay: 1,
       });
 
     }, heroSectionRef);
@@ -81,7 +99,7 @@ export default function Hero() {
       ref={heroSectionRef}
       className="h-screen relative flex items-center"
     >
-      <video ref={videoRef} autoPlay loop muted playsInline className="absolute brightness-30 inset-0 object-cover h-full w-full z-10">
+      <video ref={videoRef} autoPlay loop muted playsInline className="absolute brightness-35 inset-0 object-cover h-full w-full z-10">
         <source src="/video-hero.mp4" type="video/mp4" />
       </video>
       <div className="bg-[#141414] inset-0 absolute pointer-events-none -scale-x-100 -top-[89.9%] -z-10 w-full">
@@ -104,37 +122,29 @@ export default function Hero() {
               </span>
               <span className="text-white title-font"> FUERTE</span>
             </h1>
-            <p className="hero-subtitle text-center font-thin text-2xl sm:text-2xl text-white leading-relaxed text-pretty">
+            <p className="hero-subtitle text-center font-thin text-lg sm:text-xl text-white leading-relaxed text-pretty">
               Constructora profesional en Junín y alrededores
             </p>
             <div className="mt-10 flex items-center justify-center flex-wrap gap-2">
-              <Link
-                to="/contacto"
-                className="inline-flex rounded place-content-center bg-[#212121] hero-cta-item opacity-0 w-[169px] translate-y-5 border border-[#7a6a00] text-white hover:border-[#9c8700] hover:bg-[#1a1a1a] items-center hover:transition-colors gap-2 px-8 py-4"
-              >
-                <FiPhone size={20} />
-                Llamar
-              </Link>
-              <Link
-                to="/contacto"
-                className="inline-flex rounded bg-[#212121] hero-cta-item opacity-0 w-[169px] translate-y-5 border border-[#7a6a00] text-white hover:border-[#9c8700] hover:bg-[#1a1a1a] items-center hover:transition-colors gap-2 px-8 py-4"
-              >
-                <FaWhatsapp size={20} />
-                Whatsapp
-              </Link>
-              <Link
-                to="/contacto"
-                className="inline-flex rounded bg-[#212121] hero-cta-item opacity-0 w-[169px] translate-y-5 border border-[#7a6a00] text-white hover:border-[#9c8700] hover:bg-[#1a1a1a] items-center hover:transition-colors gap-2 px-8 py-4"
-              >
-                <FaInstagram size={20} />
-                Instagram
-              </Link>
+
+              {buttons.map((button) => (
+                <a
+                  key={button.text}
+                  href={button.href}
+                  className="inline-flex rounded group place-content-center bg-[#212121] hero-cta-item opacity-0 w-[169px] translate-y-5 border border-transparent text-white hover:border-[#9c8700] hover:bg-[#1a1a1a] items-center duration-400 transition-colors gap-2 px-8 py-4"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  {button.icon}
+                  {button.text}
+                </a>
+              ))}
+
             </div>
             <div className="items-center justify-center mt-4 flex flex-wrap gap-4">
               <Link
                 to="/trabajos"
-                data-variant="yellow"
-                className="flex rounded call-button place-content-end font-medium hero-cta-item opacity-0 w-[169px] text-center translate-y-5 items-center gap-2 px-5 py-4"
+                className="flex rounded border-b border-brand-yellow/50 call-button place-content-end font-medium hero-cta-item opacity-0 w-[169px] text-center translate-y-5 items-center gap-2 px-5 py-4"
               >
                 Ver Trabajos <FiArrowRight />
               </Link>
