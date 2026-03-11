040d79a94aea50dbf52c23affb4abe6b2accd667
Nazareno Gutierrez <nazarenojunin@gmail.com>
2026-03-10 21:31:01 -0300

    Little change

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useRef } from "react"
 import { gsap } from "gsap"
 import { ScrollTrigger } from "gsap/ScrollTrigger"
-import { projects } from "../lib/projects";
 import { Carousel, MediaItem } from "../components/Carousel";
 
 gsap.registerPlugin(ScrollTrigger)
