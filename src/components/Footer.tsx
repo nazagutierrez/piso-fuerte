442d2381e31f9cea739771a5ca8d3c2dfc598276
Nazareno Gutierrez <nazarenojunin@gmail.com>
2026-04-21 23:37:36 -0300

    little fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -29,7 +29,7 @@ export function Footer() {
   return (
     <footer className="bg-brand-dark z-20 relative text-white overflow-hidden">      
       <div className="max-w-7xl flex flex-col mx-auto px-4 sm:px-6 lg:px-8 pb-6 pt-4 sm:pt-12">
-        <div className="grid grid-cols-2 sm:grid-cols-3 gap-x-4 gap-y-8 sm:gap-8">
+        <div className={`grid ${pathname === "/" ? "grid-cols-2 sm:grid-cols-2" : "grid-cols-2 sm:grid-cols-3"} gap-x-4 gap-y-8 sm:gap-8`}>
           {/* Company Info */}
           <div className={`${pathname === "/" && "hidden"} col-span-2 sm:col-span-1 mx-auto sm:mx-0 text-center sm:text-start`}>
             <Link to="/" className="flex items-center w-fit">
